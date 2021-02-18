e9e0d6ff75211ba6fcd5a5e027544b20ea411abe
lapin.dmitriy <lapin.dmitriy@clover.globallapin.dmitriy@clover.global>
2021-02-18 22:29:06 +0300

    v 0.1 conv

--- veneer.py
+++ veneer.py
@@ -2,8 +2,10 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pywt
+import scipy.signal
 import scipy.stats as stats
-
+from scipy.signal import argrelextrema
+from scipy import ndimage
 
 def w2d(img, mode='haar', level=1):
     imArray = img
@@ -25,12 +27,69 @@ def w2d(img, mode='haar', level=1):
     imArray_H *= 255
     imArray_H = np.uint8(imArray_H)
     return imArray_H
+def cross_image(im1, im2):
 
+   im1_gray = np.sum(im1.astype('float'), axis=2)
+   im2_gray = np.sum(im2.astype('float'), axis=2)
 
+   im1_gray -= np.mean(im1_gray)
+   im2_gray -= np.mean(im2_gray)
+
+   return scipy.signal.fftconvolve(im1_gray, im2_gray[::-1,::-1], mode='same')
+
+def _rotare_img(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
+    lines = cv2.HoughLinesP(img_edges, 1, np.pi / 180.0, 100, minLineLength=100, maxLineGap=5)
+    lines_v = cv2.HoughLinesP(img_edges, 1, np.pi / 90, 100, minLineLength=100, maxLineGap=5)
+    if len(lines_v)>len(lines):
+        img = ndimage.rotate(img, 90)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
+        lines = cv2.HoughLinesP(img_edges, 1, np.pi / 180.0, 100, minLineLength=100, maxLineGap=5)
+    angles = []
+    for [[x1, y1, x2, y2]] in lines:
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
+        angles.append(angle)
+    median_angle = np.median(angles)
+    img_rotated = ndimage.rotate(img, median_angle)
+    return img_rotated
+
+def crop_ve(img):
+    img_real=img.copy()
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = np.uint8(img_gray)
+    thresh, im_bw = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    contours, x = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    all_areas = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        all_areas.append(area)
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    largest_item = sorted_contours[0]
+    x, y, w, h = cv2.boundingRect(largest_item)
+    cropped = img_real[y:y + h, x:x + w]
+    return cropped
 class Veneer():
     def __init__(self, file_name):
-        self.img_origin = cv2.imread(file_name)
+        img_origin = cv2.imread(file_name)
+        self.img_origin=_rotare_img(crop_ve(img_origin))
 
+
+    def conv_an(self):
+        mass = []
+        conv = cross_image(self.img_origin, self.img_origin)
+        for i in range(conv.shape[1]):
+            filt = conv[:, i]
+            locminarg = argrelextrema(filt, np.greater, order=5)[0]
+            mass.append(len(locminarg))
+        fit = stats.norm.pdf(mass, np.mean(mass), np.std(mass))
+        # plt.figure()
+        # plt.plot(veheer_count, fit, '-o')
+        # plt.hist(bins[:-1], bins, weights=counts / 100)
+        ans = np.ceil(mass[np.argmax(fit)])
+        return ans
     def filt_img(self):
         img = self.img_origin
         size = 8
@@ -94,10 +153,10 @@ class Veneer():
         counts, bins = np.histogram(veheer_count, bins=150)
         # print(veheer_count)
         fit = stats.norm.pdf(veheer_count, np.mean(veheer_count), np.std(veheer_count))
-        fig,ax=plt.subplots(111)
+        fig,ax=plt.subplots()
         ax.plot(veheer_count, fit, '-o')
         ax.hist(bins[:-1], bins, weights=counts / 100)
-        ans = np.ceil(veheer_count[np.argmax(fit)])
+        ans = np.ceil(veheer_count[np.argmax(fit)]/2)+1
         # print(np.ceil(veheer_count[np.argmax(fit)]))
         (h, w) = clear_matrix.shape
         center = (int(w / 2), int(h / 2))
